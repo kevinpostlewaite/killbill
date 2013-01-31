b91c4e52be50e3f60d17221fd73e02feefa2d754
stephane brossier <stephane@ning.com>
2013-01-31 15:15:30 -0800

    Hack meter service to not start. Will deploy a better way next deployment

--- meter/src/main/java/com/ning/billing/meter/DefaultMeterService.java
+++ meter/src/main/java/com/ning/billing/meter/DefaultMeterService.java
@@ -55,25 +55,32 @@ public class DefaultMeterService implements MeterService {
 
     @LifecycleHandlerType(LifecycleLevel.START_SERVICE)
     public void start() {
+        /*
         // Replay any log files that might not have been committed in the db-- should only occur if we crashed previously
         timelineEventHandler.replay(config.getSpoolDir(), internalCallContext);
-        // Start the aggregation thread
-        timelineAggregator.runAggregationThread();
+        if (config.getTimelineAggregationEnabled()) {
+            // Start the aggregation thread
+            timelineAggregator.runAggregationThread();
+        }
         // Start the backgroundDBChunkWriter thread
         backgroundDBChunkWriter.runBackgroundWriteThread();
         // Start the purger thread to delete old log files
         timelineEventHandler.startPurgeThread();
-
+*/
     }
 
     @LifecycleHandlerType(LifecycleLevel.STOP_SERVICE)
     public void stop() {
-        // Stop the aggregation thread
-        timelineAggregator.stopAggregationThread();
+        /*
+        if (config.getTimelineAggregationEnabled()) {
+            // Stop the aggregation thread
+            timelineAggregator.stopAggregationThread();
+        }
         // . Depending on shutdown mode, commit in memory timeline accumulators
         // . Will flush current direct buffer
         // . Will stop the backgroundDBChunkWriter thread
         //. Will stop the purger thread
         timelineEventHandler.commitAndShutdown(internalCallContext);
+        */
     }
 }
